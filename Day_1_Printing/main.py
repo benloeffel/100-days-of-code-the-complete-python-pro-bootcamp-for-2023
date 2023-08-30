aefc7419c0ba155697697475844fc01ae9d9ef19
Benjamin Löffel <benjaminloeffel@gmail.com>
2023-08-30 10:40:33 +0200

    feat: create band name generator

--- Day_1_Printing/main.py
+++ Day_1_Printing/main.py
@@ -1,4 +1,5 @@
 #Write your code below this line 👇
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+print("Welcome to the Band Name Generator.")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name could be " + city_name + " " + pet_name)
